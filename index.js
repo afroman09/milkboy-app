c18eaa6c0c73dfbff26cd0b766dc9a906ef04bc9
afroman09 <shinp09@gmail.com>
2021-01-10 21:26:11 +0900

    画面に描画

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-// 要素の取得完了
-// やること
-// boxがクリックされたらランダムなテキストを変えす
-// テキストは配列で保存？
 // 返されたテキストを取得して画面に反映させる処理
 // 画面に描画する時に、時間差をつける
 // 画像の表示
 
+// HTMLに取得した値を渡す
+// htmlで書くメッセージの格納先IDを定義し、
+// それぞれの関数の結果をそのIDにぶち込んで表示させる.
+
 // boxがクリックされたらランダムなテキストを表示
-document.getElementById("box").onclick = function message() {
+// document.getElementById("box").onclick = function message() {
+//   firstMessage(), secondMessage(), thirdMessage();
+// };
+
+// test
+
+document.getElementById("btn").onclick = 
+// document.getElementById("text-box").innerHTML = message();
+function message() {
   firstMessage(), secondMessage(), thirdMessage();
 };
 
+
+
 // firstMessage
 function firstMessage() {
-  console.log("あ〜、ありがとうございますぅ〜");
+  text = "あ〜、ありがとうございます〜";
+  document.getElementById("firstText").innerHTML = text;
 }
 
 // secondMessage
@@ -31,20 +42,23 @@ const secondMessageText = [
 ];
 
 // 文字列をランダムに取得
-const secondMessageNumber = Math.floor(Math.random() * secondMessageText.length);
+const secondMessageNumber = Math.floor(
+  Math.random() * secondMessageText.length
+);
 
 function secondMessage() {
   setTimeout(() => {
-    console.log(
-      "今、" + secondMessageText[secondMessageNumber] + "をいただきました〜"
-    );
+    text = "今、" + secondMessageText[secondMessageNumber] + "をいただきました〜"
+    document.getElementById("secondText").innerHTML = text;
+    // console.log(
+    //   "今、" + secondMessageText[secondMessageNumber] + "をいただきました〜"
+    // );
   }, 3000);
 }
 
 // thirdMessage (thirdTextが実行された後に実行)
 
 const thirdMessageText = [
-  "こんなんなんぼあってもいいですからね",
   "こんなんなんぼあってもいいですからね",
   "チャンジャお前は",
   "お前みたいなやつはいまだチャプチェみたことない",
@@ -55,6 +69,8 @@ const thirdMessageNumber = Math.floor(Math.random() * thirdMessageText.length);
 
 function thirdMessage() {
   setTimeout(() => {
-    console.log(thirdMessageText[thirdMessageNumber]);
+    text = thirdMessageText[thirdMessageNumber];
+    document.getElementById("thirdText").innerHTML = text;
+    // console.log(thirdMessageText[thirdMessageNumber]);
   }, 5000);
 }
